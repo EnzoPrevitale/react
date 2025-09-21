83f2d9869fb975e3b295aa0a230202c4be1706db
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-09-21 14:51:18 -0300

    imagem

--- src/components/ApiData/ApiData.tsx
+++ src/components/ApiData/ApiData.tsx
@@ -1,16 +1,30 @@
 import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function title(str: string) {
+    if(!str) return "";
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
 
 function ApiData() {
-    let dados:object;
-    axios.get("https://pokeapi.co/api/v2/pokemon/")
-    .then((response) => {
-        dados = response;
+    const [dados, setDados] = useState<any>(null);
+
+    useEffect(() => {
+        axios.get("https://pokeapi.co/api/v2/pokemon/charmander")
+        .then((response) => {
+            setDados(response.data);
+        })
+        .catch((err) => console.error(err));
+        }, []);
+
+    if(dados) {
         console.log(dados);
-    })
-    .catch((err) => console.error(err));
+    }
 
     return (
         <>
+            <p>{dados ? title(dados.name) : "Carregando..."}</p>
+            <img src=""></img>
         </>
     );
 }
