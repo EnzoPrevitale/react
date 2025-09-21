d1b2c8e896e2c0cce9721affccfc7ed17af9d41b
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-09-21 15:08:32 -0300

    tipo

--- src/components/ApiData/ApiData.tsx
+++ src/components/ApiData/ApiData.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
+import './ApiData.scss'
 
 function title(str: string) {
     if(!str) return "";
@@ -8,9 +9,10 @@ function title(str: string) {
 
 function ApiData() {
     const [dados, setDados] = useState<any>(null);
+    const [shiny, setShiny] = useState<boolean>(false);
 
     useEffect(() => {
-        axios.get("https://pokeapi.co/api/v2/pokemon/charmander")
+        axios.get("https://pokeapi.co/api/v2/pokemon/charizard")
         .then((response) => {
             setDados(response.data);
         })
@@ -24,7 +26,13 @@ function ApiData() {
     return (
         <>
             <p>{dados ? title(dados.name) : "Carregando..."}</p>
-            <img src=""></img>
+            <p className='types'>{dados?.types?.map((t: any, index: number) => (
+                <span key={index}>{title(t.type.name)}</span>
+            ))}</p>
+            { dados && (
+            <img className="pokemonImage" src={shiny ? dados.sprites.front_shiny : dados.sprites.front_default} onClick={() => setShiny(!shiny)}></img>
+            )
+            }
         </>
     );
 }
